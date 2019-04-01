fdddda79c99d97122a3fdc3a6b249b6e0edccfa0
Filipe <filipecheverrya@gmail.com>
2019-03-31 21:41:30 -0300

    thrive theme

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                use: ['svg-inline-loader']
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: ['url-loader', 'file-loader']
+            }
+        ]
+    },
+    watch: true
+};
